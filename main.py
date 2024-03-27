2edea7ad0144ae9629dfd1c7e5822e57d8671ad8
Zheng Yao Siah <49183569+zampoan@users.noreply.github.com>
2024-03-28 09:07:29 +1100

    generated predictions on sample.mp4 and sampleIRL.mp4

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8m.pt')
 
 # Open the video file
 videoPath = "sampleIRL.mp4"
@@ -13,5 +13,32 @@ cap = cv2.VideoCapture(videoPath)
 
 # Inference with custom model
 model = YOLO("runs/detect/train3/weights/best.pt")
-model.predict(source='data/data.yaml')
 # results = model.track(source=videoPath, show=True)
+
+# Training
+# yolo task=detect mode=train epochs=100 data=data/data.yaml, imgsz=640, model=yolo.v8m.pt
+
+# Predicting
+
+
+# Read frames
+while True:
+
+    if ret:
+        
+        ret, frame = cap.read()
+
+        # detect and track object
+        results = model.track(frame, persist=True)
+
+        # plot results
+        frame_ = results[0].plot()
+
+        # visualize
+        cv2.imshow('frame', frame_)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
