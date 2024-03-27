d1bfb85a9e6ac1125c1adbc4a71c81bfbb768b52
Zheng Yao Siah <49183569+zampoan@users.noreply.github.com>
2024-03-27 22:56:51 +1100

    added dataset

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import numpy as np
 import cv2
+from ultralytics import YOLO
 
+# Load the YOLOv8 model
+model = YOLO('yolov8m.pt')
+
+# Open the video file
 videoPath = "sample.mp4"
-
 cap = cv2.VideoCapture(videoPath)
+
+# Run inference
+results = model.track('sample.mp4', show=True, tracker='bytetracker.yaml')
