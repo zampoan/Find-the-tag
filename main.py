46f3e40c3b0450dda611745f01e57c4061f2fbc0
Zheng Yao Siah <49183569+zampoan@users.noreply.github.com>
2024-03-28 08:07:29 +1100

    traiined custom model

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import cv2
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO('yolov8m.pt')
+model = YOLO('yolov8n.pt')
 
 # Open the video file
-videoPath = "sample.mp4"
+videoPath = "sampleIRL.mp4"
 cap = cv2.VideoCapture(videoPath)
 
-# Run inference
-results = model.track('sample.mp4', show=True, tracker='bytetracker.yaml')
+# Train model on custom data, weights stored in runs/detect/trainX
+# results = model.train(data='data/data.yaml', epochs=25, imgsz=224,) #  device='mps'
+
+# Inference with custom model
+model = YOLO("runs/detect/train3/weights/best.pt")
+model.predict(source='data/data.yaml')
+# results = model.track(source=videoPath, show=True)
